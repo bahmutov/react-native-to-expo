844e6441bce67d3b2a8b09d7cfe2fe5badd16aa7
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-06-17 15:19:04 -0400

    working expo

--- App.js
+++ App.js
@@ -1,35 +1,25 @@
 import React, { Component } from 'react'
-import {
-  StyleSheet,
-  TouchableOpacity,
-  Text,
-  View,
-} from 'react-native'
+import { StyleSheet, TouchableOpacity, Text, View } from 'react-native'
 
 class App extends Component {
   state = {
-    count: 0
+    count: 0,
   }
 
   onPress = () => {
     this.setState({
-      count: this.state.count + 1
+      count: this.state.count + 1,
     })
   }
 
- render() {
+  render() {
     return (
       <View style={styles.container}>
-        <TouchableOpacity
-         style={styles.button}
-         onPress={this.onPress}
-        >
-         <Text>Click me</Text>
+        <TouchableOpacity style={styles.button} onPress={this.onPress}>
+          <Text>Click me</Text>
         </TouchableOpacity>
         <View>
-          <Text>
-            You clicked { this.state.count } times
-          </Text>
+          <Text>You clicked {this.state.count} times</Text>
         </View>
       </View>
     )
@@ -46,8 +36,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#DDDDDD',
     padding: 10,
-    marginBottom: 10
-  }
+    marginBottom: 10,
+  },
 })
 
-export default App;
+export default App
--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # react-native-to-expo
+
+Created a simple React Native example following this [tutorial](https://reactnative.dev/docs/tutorial).
+
+```
+$ npm i -D react react-native
++ react@17.02
++ react-native@0.64.2
+```
+
+Then added Expo using [the doc](https://docs.expo.io/guides/running-in-the-browser/)
+
+```
+$ npm i -D expo react-native-web react-dom
++ react-dom@17.0.2
++ expo@41.0.1
++ react-native-web@0.17.0
+```
+
+Remove the `main` and add a shortcut to run Expo
+
+```json
+{
+  "scripts": {
+    "start": "react-native start",
+    "web": "expo start --web"
+  },
+  "devDependencies": {
+    "expo": "^41.0.1",
+    "react": "^17.0.2",
+    "react-dom": "^17.0.2",
+    "react-native": "^0.64.2",
+    "react-native-web": "^0.17.0"
+  }
+}
+```
+
+Execute `npm run web` and the app should run at `localhost:19006`
